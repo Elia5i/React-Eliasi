0aabc8c78345c454710d68882d9ab1804a5a14fe
Elia5i <eliassinani793@gmail.com>
2025-03-04 20:50:31 +0100

    day7

---
+++ day7/screens/ListScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ListScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Welcome to List Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 15,
+    textAlign: 'center',
+    marginVertical: 20
+  }
+});
+
+export default ListScreen;
